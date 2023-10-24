c824763aa3938a3ddddf4d4c6cdeb888b7d84d74
Luiz Paulo <lpo_luiz@hotmail.com>
2023-10-24 17:06:49 -0300

    Inicial

---
+++ node_3cfadab83a3d1.tmp.js
@@ -0,0 +1,8 @@
+
+var nome = "Luiz Paulo"
+var idade = "34"
+var idade2 = "10"
+// alert("Meu nome é " + nome + ", tenho " + idade + " anos.")
+// alert(idade + idade2)
+console.log(nome)
+console.log(idade)

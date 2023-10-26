2ef91362cffe18d849dd4f909332327fe92d3c91
Luiz Paulo <lpo_luiz@hotmail.com>
2023-10-26 09:01:09 -0300

    JS

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+/*
 var nome = "Luiz Paulo"
 var idade = "34"
 var idade2 = "10"
@@ -6,3 +6,35 @@ var idade2 = "10"
 // alert(idade + idade2)
 console.log(nome)
 console.log(3 + 7)
+
+*/
+
+/* ________ARRAY________ */
+
+// var lista = ["maçã", "pêra", "laranja"]
+// console.log(lista)
+//console.log(lista[1])                   // Mostra segundo item do array
+//lista.push("uva")                       // Add um item à lista
+//lista.pop()                             // Exclui o último item da lista
+//console.log(lista.length)               // .length - mostra a qtd de elementos do array
+//console.log(lista.reverse())            // .reverse() - montra os elementos de trás pra frente
+//console.log(lista.toString())           // .toString() - mostra como String, perde a ref de array.
+//console.log(lista.join(" | "))          // .join() - Edita na hora de mostrar os elementos
+
+
+/*  ________DICIONÁRIO________  */
+
+/* var fruta = {nome: "Maçã", cor:"Vermelha"}
+   console.log(fruta.nome) 
+
+var frutas = [{nome: "Maçã", cor:"Vermelha"}, {nome: "Uva", cor:"Roxo"}]
+console.log(frutas[0].nome) */
+
+/* ________Condicionais________ */
+
+/*   var idade = prompt("Qual a sua idade? ")   // O user interege por meio do prompt. O que for colocado, será armazenado em 'var idade'
+if (idade >= 18) {
+    alert("Maior de idade")
+} else{
+    alert("Menor de idade")
+}       */
---
+++ node_429b3dd8dcc79.tmp.js
@@ -0,0 +1,13 @@
+/*
+var nome = "Luiz Paulo"
+var idade = "34"
+var idade2 = "10"
+// alert("Meu nome é " + nome + ", tenho " + idade + " anos.")
+// alert(idade + idade2)
+console.log(nome)
+console.log(3 + 7)
+
+*/
+
+var lista = ["maçã", "pêra", "laranja"]
+console.log(lista[1])
